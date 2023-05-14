b4f4fae8e1d2052580cdccc6910c429ea360d91d
FahrezaDaffa93 <117092951+FahrezaDaffa93@users.noreply.github.com>
2023-05-14 13:05:24 +0700

    login dan register

--- app/src/main/java/com/project/barbershop/RegistrasiActivity.java
+++ app/src/main/java/com/project/barbershop/RegistrasiActivity.java
@@ -1,85 +1,66 @@
 package com.project.barbershop;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import com.android.volley.Request;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.project.barbershop.servis.ServerRequestRegistrasi;
 
 public class RegistrasiActivity extends AppCompatActivity {
-    private EditText usernameEditText, passwordEditText, emailEditText, alamatEditText;
-    private Button registrasiButton;
+
+    private EditText etUsername, etPassword,etEmail,etAlamat;
+    private Button btnSubmit;
+    private ProgressDialog progressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrasi);
 
-        // Inisialisasi tampilan
-        usernameEditText = findViewById(R.id.r_username);
-        passwordEditText = findViewById(R.id.r_password);
-        emailEditText = findViewById(R.id.no_telpon);
-        alamatEditText = findViewById(R.id.alamat);
-        registrasiButton = findViewById(R.id.btnRegistrasi);
+        etUsername = findViewById(R.id.r_username);
+        etEmail = findViewById(R.id.r_email);
+        etPassword = findViewById(R.id.r_password);
 
-        // Set klik listener untuk button registrasi
-        registrasiButton.setOnClickListener(new View.OnClickListener() {
+        etAlamat= findViewById(R.id.r_alamat);
+
+        btnSubmit= findViewById(R.id.btnSubmit);
+
+
+
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Ambil nilai dari form
-                String username = usernameEditText.getText().toString().trim();
-                String password = passwordEditText.getText().toString().trim();
-                String email = emailEditText.getText().toString().trim();
-                String alamat = alamatEditText.getText().toString().trim();
+                // Ambil nilai dari edit text username dan password
+                String name = etUsername.getText().toString();
+                String email = etEmail.getText().toString();
+                String password = etPassword.getText().toString();
+                String alamat = etAlamat.getText().toString();
 
-                // Validasi input
-                if (username.isEmpty() || password.isEmpty() || email.isEmpty() || alamat.isEmpty()) {
-                    Toast.makeText(RegistrasiActivity.this, "Mohon isi semua kolom", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                // Kirim permintaan POST ke server
-                JSONObject requestBody = new JSONObject();
-                try {
-                    requestBody.put("username", username);
-                    requestBody.put("password", password);
-                    requestBody.put("email", email);
-                    requestBody.put("alamat", alamat);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-                String url = "https://example.com/api/register";
-                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, requestBody,
-                        new Response.Listener<JSONObject>() {
-                            @Override
-                            public void onResponse(JSONObject response) {
-                                Log.d("RegistrasiActivity", "response: " + response);
-                                Toast.makeText(RegistrasiActivity.this, "Registrasi berhasil", Toast.LENGTH_SHORT).show();
-                                finish();
-                            }
-                        },
-                        new Response.ErrorListener() {
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                Log.e("RegistrasiActivity", "error: " + error);
-                                Toast.makeText(RegistrasiActivity.this, "Terjadi kesalahan saat registrasi", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-
-                // Tambahkan permintaan ke antrian Volley
-                Volley.newRequestQueue(RegistrasiActivity.this).add(jsonObjectRequest);
+                // Lakukan request ke server dengan menggunakan Volley
+               ServerRequestRegistrasi serverRequest = new ServerRequestRegistrasi(RegistrasiActivity.this);
+                serverRequest.register(name,email, password,alamat, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Response berhasil diterima, lakukan aksi yang diperlukan
+                        Toast.makeText(RegistrasiActivity.this, "Registrasi berhasil", Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Terjadi error saat melakukan request ke server
+                        Toast.makeText(RegistrasiActivity.this, "Terjadi kesalahan: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
     }
 }
-
---
+++ app/src/main/java/com/project/barbershop/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.project.barbershop;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash_screen);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                finish();
+            }
+        }, 3000L);
+
+    }
+}
